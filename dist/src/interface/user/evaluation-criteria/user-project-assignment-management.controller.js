e7bce9c37decaffee17f4b658a05875aff3a54ca
= <wukddang@gmail.com>
2025-12-15 20:21:40 +0900

    feat: 프로젝트 할당 생성 기능 추가
    - 사용자 프로젝트 할당 관리 컨트롤러에 프로젝트 할당 생성 기능 구현
    - 프로젝트 할당 생성 시 필요한 DTO 및 사용자 인증 정보 처리 로직 추가
    - 할당 가능한 프로젝트 목록 조회 기능과 함께 사용자 인터페이스 개선

--- dist/src/interface/user/evaluation-criteria/user-project-assignment-management.controller.js
+++ dist/src/interface/user/evaluation-criteria/user-project-assignment-management.controller.js
@@ -13,7 +13,9 @@ var __param = (this && this.__param) || function (paramIndex, decorator) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserProjectAssignmentManagementController = void 0;
+const project_assignment_business_service_1 = require("../../../business/project-assignment/project-assignment-business.service");
 const evaluation_criteria_management_service_1 = require("../../../context/evaluation-criteria-management-context/evaluation-criteria-management.service");
+const current_user_decorator_1 = require("../../common/decorators/current-user.decorator");
 const decorators_1 = require("../../common/decorators");
 const project_assignment_api_decorators_1 = require("../../common/decorators/evaluation-criteria/project-assignment-api.decorators");
 const project_assignment_dto_1 = require("../../common/dto/evaluation-criteria/project-assignment.dto");
@@ -21,8 +23,19 @@ const common_1 = require("@nestjs/common");
 const swagger_1 = require("@nestjs/swagger");
 let UserProjectAssignmentManagementController = class UserProjectAssignmentManagementController {
     evaluationCriteriaManagementService;
-    constructor(evaluationCriteriaManagementService) {
+    projectAssignmentBusinessService;
+    constructor(evaluationCriteriaManagementService, projectAssignmentBusinessService) {
         this.evaluationCriteriaManagementService = evaluationCriteriaManagementService;
+        this.projectAssignmentBusinessService = projectAssignmentBusinessService;
+    }
+    async createProjectAssignment(createDto, user) {
+        const assignedBy = user.id;
+        return await this.projectAssignmentBusinessService.프로젝트를_할당한다({
+            employeeId: createDto.employeeId,
+            projectId: createDto.projectId,
+            periodId: createDto.periodId,
+            assignedBy: assignedBy,
+        }, assignedBy);
     }
     async getAvailableProjects(query) {
         const result = await this.evaluationCriteriaManagementService.할당_가능한_프로젝트_목록을_조회한다(query.periodId, {
@@ -48,6 +61,14 @@ let UserProjectAssignmentManagementController = class UserProjectAssignmentManag
 };
 exports.UserProjectAssignmentManagementController = UserProjectAssignmentManagementController;
 __decorate([
+    (0, project_assignment_api_decorators_1.CreateProjectAssignment)(),
+    __param(0, (0, common_1.Body)()),
+    __param(1, (0, current_user_decorator_1.CurrentUser)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [project_assignment_dto_1.CreateProjectAssignmentDto, Object]),
+    __metadata("design:returntype", Promise)
+], UserProjectAssignmentManagementController.prototype, "createProjectAssignment", null);
+__decorate([
     (0, project_assignment_api_decorators_1.GetAvailableProjects)(),
     __param(0, (0, common_1.Query)()),
     __metadata("design:type", Function),
@@ -59,6 +80,7 @@ exports.UserProjectAssignmentManagementController = UserProjectAssignmentManagem
     (0, swagger_1.ApiBearerAuth)('Bearer'),
     (0, decorators_1.Roles)('user'),
     (0, common_1.Controller)('user/evaluation-criteria/project-assignments'),
-    __metadata("design:paramtypes", [evaluation_criteria_management_service_1.EvaluationCriteriaManagementService])
+    __metadata("design:paramtypes", [evaluation_criteria_management_service_1.EvaluationCriteriaManagementService,
+        project_assignment_business_service_1.ProjectAssignmentBusinessService])
 ], UserProjectAssignmentManagementController);
 //# sourceMappingURL=user-project-assignment-management.controller.js.map
